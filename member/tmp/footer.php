7080b5392d4e470b76272d6d0a7bc5b8401fb6c9
Takuro Yoshimi <>
2025-11-08 22:36:48 +0900

    コピーライトの変更

--- member/tmp/footer.php
+++ member/tmp/footer.php
@@ -1,4 +1,4 @@
   <footer>
   <div class="Back"><a href="#wrapper"><p class="Hv"><img src="/common/img/backArrow.png" width="39" height="22" alt=""/></p></a></div>
-  <div class="Copy"><p>©︎ Sparkle Inc.</p></div>
+  <div class="Copy"><p>©︎  <?php echo date('Y'); ?> Sparkle Inc.</p></div>
   </footer>
